ce658792ad3aae6d43c27584173135b4283c7ff1
Troy Martin <troy.martin@techneaux.com>
2024-07-30 15:48:34 -0500

    3:41pm 7/30/24 updated html js and css files to include the carousel. Troubleshooting re

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ hamburger.addEventListener('click', () => {
 
 let slideIndex = 1;
 showSlides(slideIndex);
+
+/*controls*/
+function advanceSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides")
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for(i=0; i < dots.length; i++) {
+        slides[i].computedStyleMap.display = "none";
+    } for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
